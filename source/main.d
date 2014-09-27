b5daff3742a262de05ec24ad270e4e0ef1820767
logaras <logaras@gmail.com>
2014-09-27 22:53:29 +0300

    Sketching fields operation

--- source/main.d
+++ source/main.d
@@ -14,6 +14,9 @@ void main(string[] args)
    
    // Should perform the field operation
    bool fOperation = false;
+   int cutFieldStart = 0;
+   int cutFieldStop = 0;
+   char delimiter = '\t';
    
    // Should perform the delimiter-based field operation
    bool dOperation = false;
@@ -48,32 +51,70 @@ void main(string[] args)
    		}
    		}
    	
+   	int fIndex = arg.indexOf("-f");
+   	if(fIndex == 0 ){
+   		
+   		fOperation = true;
+   		string fArg = arg["-b".length .. arg.length];
+   		
+   		// This is a single byte, not from-to
+   		if(fArg.indexOf("-")==-1){
+   			cutFieldStart = to!int(fArg);
+   			cutFieldStop = cutFieldStart;
+   			
+   			}
+   		else{
+   			cutFieldStart = to!int(fArg.split("-")[0]);
+   			cutFieldStop = to!int(fArg.split("-")[1]);
+   		}
+   		
+   		}
+   	
+   	
    	if(cutByteStart>cutByteStop){
    		writefln("Error!");
    		return;
-   		}	
+   		}
+   	
+   	if(cutFieldStart>cutFieldStop){
+   		writefln("Error!");
+   		return;
+   		}		
    	
    	}
    writefln("b: %s %s %s",bOperation,cutByteStart,cutByteStop);
-   
+   writefln("f: %s %s %s",fOperation,cutFieldStart,cutFieldStop);
 
-   
-   writefln("********** %s ********** \n",filename);
 	
 	foreach( line; File(filename).byLine())
 	{
+		if(bOperation){
 		int lineByteStart = cutByteStart;
 		int lineByteStop = cutByteStop;
 
 		if(lineByteStop > line.length){
+			writefln("1");
 			lineByteStop = line.length;
 			}
 		if(lineByteStart >line.length){
+			writefln("2");
 			lineByteStart = line.length;
 			}
 		
-		writeln(line[lineByteStart ..  lineByteStop]);
+		writeln(line[lineByteStart-1 ..  lineByteStop]);
+		}
+		
+		
+		if(fOperation){
+			int fieldsRange = cutFieldStop - cutFieldStop; 
+			for(int fieldInd = cutFieldStart-1; fieldInd<cutFieldStop;fieldInd++){
+				writef(line.split(delimiter)[fieldInd]);
+				writef("\t");		
+				}
+			 
+			}
+		writefln("");
 	}
-	writeln("stop");
+	
 
 }
